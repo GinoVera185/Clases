7d158faddbfb56e85d66a2dcc7c4fa58210258ee
Gino_Vera <ginovera33@gmail.com>
2025-09-11 10:57:41 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string miVariable;
+int edad;
+
+Console.WriteLine("Ingrese su nombre: ");
+miVariable = Console.ReadLine();
+Console.WriteLine("Ingrese su edad: ");
+edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hola soy {miVariable}, tengo {edad} años");
